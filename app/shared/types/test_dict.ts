4fa8df3f8691737e45d30aea15b768ce73e82ffa
gfffrtt <geogueserporque@gmail.com>
2024-02-26 21:08:59 -0300

    fix: build errors v2

---
+++ app/shared/types/test_dict.ts
@@ -0,0 +1,8 @@
+import { $Enums } from "@prisma/client";
+
+export const TEST_DICT = {
+  AR: $Enums.ELEMENTS.AIR,
+  WA: $Enums.ELEMENTS.WATER,
+  FI: $Enums.ELEMENTS.FIRE,
+  EA: $Enums.ELEMENTS.EARTH,
+};
